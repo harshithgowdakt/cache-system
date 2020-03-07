32f79a3b2cac60eeb4e48b8ccaab61eb6521519f
Harshith Gowda <harshithgowdakt@gmail.com>
2020-03-07 23:01:35 +0530

    done assignment using sockets as well

---
+++ assignment-1-tcp-server/client.cpp
@@ -0,0 +1,82 @@
+#include <iostream> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <unistd.h> 
+#include <string.h>
+#include <nlohmann/json.hpp>
+
+#define _XOPEN_SOURCE_EXTENDED 1 
+#define PORT 8080
+
+using namespace std;
+using json = nlohmann::json;
+   
+class Client{
+private:
+    int socket_fd; 
+    struct sockaddr_in server_address; 
+    char data[1024] = R"({"command":"GET", "key":"id", "value":"harshith"})"; 
+
+public:
+    Client(){
+        server_address.sin_family = AF_INET; 
+        server_address.sin_port = htons(PORT);
+    }
+
+    void create_socket(){
+        socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	    if (socket_fd < 0){ 
+            cout<<"Failed to create socket \n";
+            exit(0);
+        }
+        cout<<"Socket created successfully \n";
+    }
+
+    void convert_stringIP_to_numericIP(){
+        if(inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr)<=0) { 
+            cout<<"Invalid address \n";
+            close_socket();
+        }
+    }
+
+    void connet_to_server(){
+        if (connect(socket_fd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) { 
+            cout<< "Couldn't connect server \n";
+            close_socket();
+        } 
+        cout<<"Connected to the server \n";
+    }
+
+    void send_and_recieve_data(){
+        char buffer[1024];
+        cout<<"Sending request to server \n";
+        if ((send(socket_fd, data, strlen(data), 0)) > 0){
+            if ((recv(socket_fd, buffer, 10240, 0)) > 0){
+                cout << "Response:: " << buffer << "\n";
+            }else{
+                cout << "Couldn't receive response \n";
+                close_socket();
+            }
+        }else{
+            cout << "Couldn't send the request \n";
+            close_socket();
+        }
+    }
+
+    void close_socket(){       
+		close(socket_fd);
+		exit(0);
+	}
+};
+
+
+int main() { 
+    Client client;
+
+    client.create_socket();
+    client.convert_stringIP_to_numericIP();
+    client.connet_to_server();
+    client.send_and_recieve_data();
+
+    return 0; 
+} 
---
+++ assignment-1-tcp-server/server.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h> 
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <nlohmann/json.hpp>
+#include <map>
+
+#define _XOPEN_SOURCE_EXTENDED 1
+#define PORT 8080
+
+using namespace std;
+using json = nlohmann::json;
+
+class Server{
+
+private:
+	int socket_fd,socket_fd_client;
+	struct sockaddr_in socket_address;
+	int address_lenth = sizeof(socket_address);
+	char buffer[1024];
+	map<string, string> list;
+
+public:
+	Server(){
+		socket_address.sin_family = AF_INET; 
+   		socket_address.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+    	socket_address.sin_port = htons( PORT ); 
+	}
+
+	void create_socket(){
+		socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+		if(socket_fd < 0){
+			cout<<"Couldn't create socket \n";
+			close(socket_fd);
+		}
+		cout<<"Socket created successfully\n";
+	}
+
+	void bind_address(){
+		if(bind(socket_fd, (struct sockaddr *)&socket_address, address_lenth)<0){
+			cout<< "Couldn't bind the IP address \n";
+			close(socket_fd);
+		}
+		cout<<"Address binding completed \n";	
+	}
+
+	void listen_to_request(){
+		if ((listen(socket_fd,0))<0){
+			cout<<"Couldn't listen to incoming request";
+			close(socket_fd);
+		}	
+		cout<<"Listening to incoming request....... \n";	
+	}
+
+	void accept_connection(){
+		socket_fd_client = accept(socket_fd, (struct sockaddr *)&socket_address,(socklen_t*)&address_lenth);
+		if (socket_fd_client < 0) { 
+			cout << "Couldn't accept the connection \n"; 
+			close(socket_fd);
+		} 
+		cout<<"Connection accepted \n";
+	}
+
+	bool is_key_exists(string key){
+		map<string, string>::iterator i;
+		for (i = list.begin(); i != list.end(); ++i){
+			if (i->first == key){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	string save_data(string key, string value){
+		if(!is_key_exists(key)){
+			list.insert(pair<string, string>(key, value));
+			return "OK";
+		}else{
+			return "key already exists";
+		}
+	}
+
+	string get_data(string key){
+		map<string, string>::iterator i;
+		for (i = list.begin(); i != list.end(); ++i){
+			if (i->first == key){
+				return i->second;
+			}
+		}
+		return "key does not exists";
+	}
+
+	string process_data(json data){
+		string response;
+		if (data["command"]=="GET"){
+			response =	get_data(data["key"]);
+		}else if(data["command"]=="SET"){
+			response = save_data(data["key"], data["value"]);
+		}
+		return response;
+	}
+
+	void recieve_and_send(){
+		json data; string response;
+		memset(buffer,0,sizeof(buffer));
+		if(recv(socket_fd_client, buffer, 1024, 0) > 0){
+			cout << "Request ::" << buffer << "\n";
+			data = json::parse(buffer);
+		 	response = process_data(data);
+			 if ((send(socket_fd_client, response.c_str(), strlen(response.c_str()), 0)) < 0){
+				close_socket();
+				cout<< "Error while sending response \n";
+			}	
+		}else{
+			close_socket();
+			cout<< "Error while receiving request \n";
+		}
+	}
+
+	void close_socket(){       
+		close(socket_fd);
+		exit(0);
+	}
+};
+
+
+int main(){
+	Server server;
+
+	server.create_socket();
+	server.bind_address();
+	server.listen_to_request();
+	while (true){
+		server.accept_connection();
+		server.recieve_and_send();
+	}
+	server.close_socket();
+	return 0;
+}
